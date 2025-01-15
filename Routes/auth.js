94c67db643ce52de7fef1685c6095eb445fc9e07
Dhirubhai Kulkarni <kulkarnidhirubhai@gmail.com>
2025-01-15 17:03:18 +0530

    Created Node server with mongodb connection and Added Login and Register User API.

---
+++ Routes/auth.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const ObjectID = require('mongodb').ObjectId;
+const jwt = require("jsonwebtoken");
+const { encrypt, decrypt } = require("../security/crypto");
+
+const checkValueEmptyOrNull = (value) => {
+    return (value === undefined || value === null || value === "" || value === "NULL" || value === "null") ? false : true;
+}
+
+router.post("/login", async (req, res) => {
+    try {
+        const dbConnection = await global.clientConnection;
+        const db = await dbConnection.db("AurusCodeChallenge");
+        const users = await db.collection("Users");
+
+        const user = await users.findOne({ email: req.body.email });
+        if (!checkValueEmptyOrNull(user)) {
+            return res.status(200).send({ error: { code: "Failed", message: "User Does Not Exist credentials" } });
+        }
+
+        const compare = await bcrypt.compare(req.body.password, user.password);
+        if (!compare) {
+            return res.status(200).send({ error: { code: "Failed", message: "Incorrect credentials" } });
+        }
+
+        const token = jwt.sign({
+            id: user._id,
+            role: user.role,
+            email: user.email,
+            username: user.username
+
+        },
+            process.env.JWT_PRIVATEKEY,
+            {
+                expiresIn: process.env.JWT_EXPIRESIN
+            }
+        );
+
+        let userData = {
+            user,
+            token
+        }
+        res.status(200).send(userData); // Send the user data on successful login
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Failed");
+    }
+});
+
+
+
+module.exports = router;
---
+++ Routes/users.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const ObjectID = require('mongodb').ObjectId;
+
+router.post("/register", async (req, res) => {
+    try {
+        const dbConnection = await global.clientConnection;
+        const db = await dbConnection.db("AurusCodeChallenge");
+        const users = await db.collection("Users");
+
+        // Check if user with provided email exists
+        const existingEmailUser = await users.findOne({ email: req.body.email });
+        if (existingEmailUser) {
+            return res.status(400).send("Email is already registered.");
+        }
+
+        // Check if user with provided username exists
+        const existingUsernameUser = await users.findOne({ username: req.body.username });
+        if (existingUsernameUser) {
+            return res.status(400).send("Username is already taken.");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+        const user = {
+            password: hashPassword,
+            email: req.body.email,
+            role: "user",
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            contactNumber: req.body.contactNumber,
+            createdAt: new Date()
+        };
+
+        let result = await users.insertOne(user);
+        res.status(200).send("Success");
+    } catch (error) {
+        res.status(500).send("Failed");
+    }
+});
+
+
+
+
+module.exports = router;
